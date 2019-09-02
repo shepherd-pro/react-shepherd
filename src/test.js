94adfabf20dc7daf67f71a7c080fe3ce9b159d9a
Chuck Carpenter <chuck.carpenter@me.com>
2019-09-02 13:58:12 -0700

    add basic test for render and buttons

--- src/test.js
+++ src/test.js
@@ -5,67 +5,51 @@ import { ShepherdTour, TourMethods } from './';
 const steps = [
   {
     id: 'welcome',
-    options: {
-      text: [
-        `
-        Shepherd is a JavaScript library for guiding users through your app.
-        It uses <a href="https://atomiks.github.io/tippyjs//">Tippy.js</a>,
-        another open source library, to render dialogs for each tour "step".
-      `,
-        `
-        Among many things, Tippy makes sure your steps never end up off screen or cropped by an overflow.
-        Try resizing your browser to see what we mean.
+    text: [
       `
-      ],
-      attachTo: '.hero-welcome bottom',
-      classes: 'shepherd shepherd-welcome',
-      buttons: [
-        {
-          type: 'cancel',
-          classes: 'shepherd-button-secondary',
-          text: 'Exit'
-        },
-        {
-          type: 'next',
-          text: 'Next'
-        }
-      ]
-    }
+      Shepherd is a JavaScript library for guiding users through your app.
+      It uses <a href="https://atomiks.github.io/tippyjs//">Tippy.js</a>,
+      another open source library, to render dialogs for each tour "step".
+    `,
+      `
+      Among many things, Tippy makes sure your steps never end up off screen or cropped by an overflow.
+      Try resizing your browser to see what we mean.
+    `
+    ],
+    classes: 'shepherd shepherd-welcome',
+    buttons: [
+      {
+        type: 'cancel',
+        classes: 'shepherd-button-secondary',
+        text: 'Exit'
+      },
+      {
+        type: 'next',
+        text: 'Next'
+      }
+    ]
   }
 ];
-const defaultStepOptions = { showCancelLink: true };
-const useModalOverlay = true;
-
-// function Content() {
-//   return (
-//     <div>
-//       <HomePage>
-//         <TourMethods>
-//           {({ startTour }) => (
-//             <button className="button dark" onClick={startTour}>
-//               Start Tour
-//             </button>
-//           )}
-//         </TourMethods>
-//       </HomePage>
-//     </div>
-//   );
-// }
+const tourOptions = {
+  defaultStepOptions: { showCancelLink: true },
+  useModalOverlay: true
+};
 
 describe('<ShepherdTour />', () => {
   it('exists', () => {
     expect(ShepherdTour).toBeTruthy();
   });
 
-  it('renders the component', () => {
+  it('renders the component and starts tour', async () => {
     const container = render(
-      <ShepherdTour
-        defaultStepOptions={defaultStepOptions}
-        steps={steps}
-        useModalOverlay={useModalOverlay}
-      />
+      <ShepherdTour steps={steps} tourOptions={tourOptions}>
+        <TourMethods>{context => <div>{context.start()}</div>}</TourMethods>
+      </ShepherdTour>
     );
+    const cancelBtn = await container.findByText('Exit');
+    const nextBtn = await container.findByText('Next');
 
-    expect(container.firstChild).toMatchSnapshot();
+    expect(cancelBtn).toBeTruthy();
+    expect(nextBtn).toBeTruthy();
   });
 });
