d93c7b075b13f068734ef199299cbb21eb02dcd3
Chuck Carpenter <chuck.carpenter@me.com>
2019-09-02 13:36:14 -0700

    refactor to v5 syntax and less private methods for setup

--- src/test.js
+++ src/test.js
@@ -1,7 +1,71 @@
-import { ShepherdTour } from './';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { ShepherdTour, TourMethods } from './';
 
-describe('ShepherdTour', () => {
-  it('is truthy', () => {
+const steps = [
+  {
+    id: 'welcome',
+    options: {
+      text: [
+        `
+        Shepherd is a JavaScript library for guiding users through your app.
+        It uses <a href="https://atomiks.github.io/tippyjs//">Tippy.js</a>,
+        another open source library, to render dialogs for each tour "step".
+      `,
+        `
+        Among many things, Tippy makes sure your steps never end up off screen or cropped by an overflow.
+        Try resizing your browser to see what we mean.
+      `
+      ],
+      attachTo: '.hero-welcome bottom',
+      classes: 'shepherd shepherd-welcome',
+      buttons: [
+        {
+          type: 'cancel',
+          classes: 'shepherd-button-secondary',
+          text: 'Exit'
+        },
+        {
+          type: 'next',
+          text: 'Next'
+        }
+      ]
+    }
+  }
+];
+const defaultStepOptions = { showCancelLink: true };
+const useModalOverlay = true;
+
+// function Content() {
+//   return (
+//     <div>
+//       <HomePage>
+//         <TourMethods>
+//           {({ startTour }) => (
+//             <button className="button dark" onClick={startTour}>
+//               Start Tour
+//             </button>
+//           )}
+//         </TourMethods>
+//       </HomePage>
+//     </div>
+//   );
+// }
+
+describe('<ShepherdTour />', () => {
+  it('exists', () => {
     expect(ShepherdTour).toBeTruthy();
+  });
+
+  it('renders the component', () => {
+    const container = render(
+      <ShepherdTour
+        defaultStepOptions={defaultStepOptions}
+        steps={steps}
+        useModalOverlay={useModalOverlay}
+      />
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
