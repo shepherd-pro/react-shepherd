81f7980769e0fcced0eac31e1dda44a1144f62e5
Chuck Carpenter <chuck.carpenter@me.com>
2019-09-04 06:13:08 -0700

    refactor to use TS

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,16 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
-import svgr from '@svgr/rollup'
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import resolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript';
+import url from 'rollup-plugin-url';
+import svgr from '@svgr/rollup';
 
-import pkg from './package.json'
+import pkg from './package.json';
 
 export default {
-  input: 'src/index.js',
+  input: 'src/index.tsx',
   output: [
     {
       file: pkg.main,
@@ -23,6 +24,7 @@ export default {
     }
   ],
   plugins: [
+    typescript(),
     external(),
     postcss({
       modules: true
@@ -35,4 +37,4 @@ export default {
     resolve(),
     commonjs()
   ]
-}
+};
